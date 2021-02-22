ef5c20b50dee742879c61f77c89b1c686f81bf37
joghue <joghue@github.com>
2021-02-21 21:09:49 -0300

    Update Arquivos_quadrados.py

--- Arquivos_quadrados.py
+++ Arquivos_quadrados.py
@@ -25,19 +25,11 @@ lista_6 =  [-4,11]
 lista_7=[-2,16]
 lista_8 = [2,55]
 lista_9 = [-2,52]
-Solution,Count,tuplex,error = sec(quadrado(lista_2),cd.initial,cd.second,cd.tol1,cd.intmax,cd.tol3)
+Solution,Count,tuplex,error = sec(quadrado(lista_3),cd.initial,cd.second,cd.tol1,cd.intmax,cd.tol3)
 k = len(tuplex)
 i = 0
 j = 0
-for i in range (0,k):
-    for j in range(0,k):
-        if tuplex[i] < tuplex[j]:
-            temp = tuplex[i]
-            tuplex[i] = tuplex[j]
-            tuplex[j] = temp
-        else:
-            continue
-DesvioPadrão = np.std(tuplex, axis = 0, dtype = float)
+
 gr.plot_quad(lista_1,Solution,"fig")
 gr.plot_Analisequad(lista_0,Solution,quadrado(lista_3),tuplex,"fig2")
 gr.plot_erro(error,"fig4")
@@ -45,4 +37,4 @@ print(Solution)
 print(tuplex)
 print(Count)
 print(error)
-print(DesvioPadrão)              
+         
