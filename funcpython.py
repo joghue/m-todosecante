31577ef1e24d569fcad6de120c233aa68a1a4502
joghue <joghue@github.com>
2021-02-21 21:09:37 -0300

    Update funcpython.py

--- funcpython.py
+++ funcpython.py
@@ -5,35 +5,37 @@ Created on Thu Nov 19 20:06:27 2020
 @author: joaqu
 """
 import math
-def sec(fun, x,h,tol,IterMax,tolfunc):                    
+def sec(fun, x,b,tol,IterMax,tolfunc):                    
     f_a = fun(x)
-    f = fun(h)
-    EA = abs(x-h)
+    f_b = fun(b)
+    EA = abs(x-b)
     Contador = 0     
     listax=  []
     erros = []
     listax.append(x)
-    listax.append(h)
+    listax.append(b)
     erros.append(EA)
-    while Contador < IterMax and tol*abs(h)<=EA and abs(f_a)>tolfunc:
+    funcoes = [f_a,f_b]
+    while Contador < IterMax and tol*abs(b)<=EA and abs(f_a)>tolfunc:
         try:
            Contador += 1 
-           x_1 = (f*x - f_a*h)/(f_a- f)
-           x = h
-           h = x_1
-           f_a = f
-           f = fun(h)
-           EA = abs(x-h)        
+           x_1 = (f_b*x - f_a*b)/(f_b- f_a)
+           x = b
+           b = x_1
+           f_a = f_b
+           f_b = fun(b)
+           EA = abs(x-b)        
            listax.append(x_1)
-           erros.append(EA)               
+           erros.append(EA)
+           funcoes.append(f_a)
         except(ZeroDivisionError):
-            x_1 = (x + h)/2
+            x_1 = (x + b)/2
     else:
             if(Contador>IterMax):
                 print("Número de iterações ultrapassdas")
-            if(tol*abs(h)>EA):
+            if(tol*abs(b)>EA):
                 print("tolerância atingida")
             if(abs(f_a)<tolfunc):
                 print("tolerância da função")
-    return x_1,Contador,listax,erros
+    return x_1,Contador,listax,erros,funcoes
     
