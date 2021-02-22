f2d2ecb0ab5a2de0bb79e7d2e4c524c15a3daec8
joghue <joghue@github.com>
2021-02-21 21:09:54 -0300

    Update condicoes.py

--- condicoes.py
+++ condicoes.py
@@ -6,7 +6,7 @@ Created on Mon Dec  7 00:23:33 2020
 """
 # """condicoes utilizadas globalmente"""
 # 'condicoes = [x,h,tolerancia,itermax,tolfunc]'
-condicoes= [0,1,0.00001,100000,0.0001]
+condicoes= [-4021.5,-2659,0.00001,100000,0.0001]
 initial  = condicoes[0]
 second = condicoes[1]
 tol1 = condicoes[4]
