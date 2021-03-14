a92e38ce93a74ee6ce7b8dee7ed0831cf5e882bb
joghue <joghue@github.com>
2021-03-14 16:50:48 -0300

    Update Arquivos_quadrados.py

--- Arquivos_quadrados.py
+++ Arquivos_quadrados.py
@@ -9,10 +9,11 @@ def quadrado(lista):
     return lambda x:max(0,abs(x+lista[1]-lista[0]))*max(0,abs(lista[0]+lista[1]-x))
 
 
-import condicoes as cd
+import condicoes as cond
 import grafico as gr
 from funcpython import sec 
-
+import feradores as fe
+import numpy as np
 
 lista_0=[3,4]
 lista_1=[3,12]
@@ -24,10 +25,25 @@ lista_6 =  [-4,11]
 lista_7=[-2,16]
 lista_8 = [2,55]
 lista_9 = [-2,52]
-Solution,Count,tuplex,error,funcoes = sec(quadrado(lista_7),cd.initial,cd.second,cd.tol1,cd.intmax,cd.tol3)
-k = len(tuplex)
+
 i = 0
 j = 0
+file_name = "resultado.quadrado.tex"
+with open(file_name,"w") as file_object:
+ for i in range(0,4):
+    str(i)
+    Solution,Count,tuplex,error,funcoes = sec(quadrado(lista_0),fe.intervalos[i],fe.intervalos[i+1],cond.intmax,cond.tol1,cond.tol3) 
+    
+    DesvioPadrão = np.std(tuplex, axis = 0, dtype = float)
+    print(Solution)
+    A_1 = str(round(fe.intervalos[i],3))
+    A_2 = str(round(fe.intervalos[i+1],3))  
+    A = str(round(Solution,3))
+    B = str(round(Count,3))
+
+    D = str(round(DesvioPadrão,3))
+    a = ("[" + A_1 + "; " + A_2 + "]" + "& " + A + "& " + B + "& " + D  + "\n "  )
+    file_object.write(a)
 
 gr.plot_quad(lista_3,Solution,"fig")
 gr.plot_Analisequad(lista_3,Solution,quadrado(lista_3),tuplex,"fig2")
