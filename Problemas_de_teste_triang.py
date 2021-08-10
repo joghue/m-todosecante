cd1ae707d0e4d096b25e03d121ddb5a158c7b4a5
joghue <joghue@github.com>
2021-08-10 14:25:38 -0300

    Update Problemas_de_teste_triang.py

--- Problemas_de_teste_triang.py
+++ Problemas_de_teste_triang.py
@@ -11,12 +11,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from grafico import triplot
 from MS_Caso_convexo import Sol_convex
-from grafico import plot_solucao
+from grafico import plot_solucao, teste
 from secante_adulterada import Sol_Triangle
 from grafico import Analise_tri
 # LT_0 = [yA,l1A,l2A,z,theta,xB,yB,l1B,l2B,a1B,a2B]
-LT_0_FIXO =[0,5,5,45,0,5.8]
-LT_0 = [[0,3.2,2.5,30,104],[22,33,0,55,66,12]]
 LT_1 = [0,5,5,60,0,3.5,1.9,2,2,30,104]
 LT_2 = [0,5,5.8,56,0,3.5,0,3.9,2,30,104]
 LT_3 = [0,5,2.9,137,0,1.1,0,2.1,2,30,104]
@@ -26,11 +24,13 @@ LT_6 = [1.6,1.4,2.4,45,212,0,0,2.1,2,49,336]
 LT_7 = [2.3,2.,4.5,212,0,1.6,0,2.1,2,49,336]
 LT_8 = [1.3,2.9,4.7,212,0,1.6,1.7,3,2,49,300]
 LT_9 = [1.9, 2.9,4.7,212,0,1,2.2,3.1,5,49,180]
+# a,b,c,d,e,f= Sol_Triangle(1.8, 1, cond.intmax,cond.tol1,cond.tol3,LT_0_FIXO,LT_1)
 a,b,c,d,e,f= Sol_Triangle(1.8, 1, cond.intmax,cond.tol1,cond.tol3,LT_1)
 print(a)
 print(a)
-file_name = "resultado.Tri[0].tex"
 Analise_tri(c,e)
+file_name = "resultado.Tri[0].tex"
+# Analise_tri(c,e)
 
 # mtri(x,y,triangulation)
 # plt.triplot(mtri)
